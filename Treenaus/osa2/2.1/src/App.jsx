40d578014239eb6f0608013ab9cbae39be342547
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-06-24 18:47:08 +0300

    training

--- Treenaus/osa2/2.1/src/App.jsx
+++ Treenaus/osa2/2.1/src/App.jsx
@@ -1,70 +1,47 @@
-import React from 'react';
+const Header = ({course}) => (
+  <h1>{course}</h1>
+)
 
-const Header = ({course}) => {
-  return <h1>{course.name}</h1>;
-};
+const Part = ({part, exercises}) => (
+  <p>{part} {exercises}</p>
+)
 
-const Content = ({course}) => {
-  return (
+const Content = ({part1, part2, part3, exercises1, exercises2, exercises3}) => {
+  return(
     <div>
-      {course.parts.map(part => (
-        <Part key={part.id} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Part = ({part, exercises}) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-};
-
-const Total = ({course}) => {
-  const totalExercises = course.parts.reduce( (sum, part) => 
-     sum + part.exercises, 0);
-
-  return( <p>Total exercises: {totalExercises}</p> 
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+  </div>
   )
 }
 
+const Total = ({exercises1, exercises2, exercises3}) => {
+return(
+  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+)
+}
+
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4, 
-      },
-    ],
-  };
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Content 
+      part1={part1} exercises1={exercises1}
+      part2={part2} exercises2={exercises2}
+      part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
