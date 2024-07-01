beb293c1c4b22435e63a87982d38f1594d296dc8
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-01 16:18:14 +0300

    Update index.js

--- osa3_puhelinluettelo/index.js
+++ osa3_puhelinluettelo/index.js
@@ -25,7 +25,22 @@ let persons = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+  }
+
 app.use(express.json())
+app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+  
+  app.use(unknownEndpoint)
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
