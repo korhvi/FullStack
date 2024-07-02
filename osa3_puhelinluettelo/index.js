a819362490418f6e6423b1cfe94c7fcda4c545fb
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-02 17:55:01 +0300

    Update index.js

--- osa3_puhelinluettelo/index.js
+++ osa3_puhelinluettelo/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const port = process.env.PORT || 3001
 
 
 
@@ -115,7 +116,7 @@ app.get('/info', (request, response) => {
   })
 
 
-const port = process.env.PORT || 3001
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
