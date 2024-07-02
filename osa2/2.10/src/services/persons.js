1e695c202eeeda18e7fc73287ad1ac7a9355e408
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-02 17:11:17 +0300

    Update persons.js

--- osa2/2.10/src/services/persons.js
+++ osa2/2.10/src/services/persons.js
@@ -1,20 +1,24 @@
 import axios from 'axios'
-const baseUrl = '/api/persons'
+const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  return axios.get(baseUrl).then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
 const create = newObject => {
-  return axios.post(baseUrl, newObject).then(response => response.data)
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
-  return axios.put(`${baseUrl}/${id}`, newObject).then(response => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
 }
 
 const remove = (id) => {
-  return axios.delete(`${baseUrl}/${id}`).then(response => response.data)
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then(response => response.data)
 }
 
 
