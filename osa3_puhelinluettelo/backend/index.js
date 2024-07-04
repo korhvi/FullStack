eee05b30bc0a3493665deedd603dff8506d1713b
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-04 09:59:35 +0300

    fixed if bodyname to person.js

--- osa3_puhelinluettelo/backend/index.js
+++ osa3_puhelinluettelo/backend/index.js
@@ -19,7 +19,9 @@ const errorHandler = (error, request, response, next) => {
   
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
 
   next(error)
 }
@@ -45,15 +47,9 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing' 
-    })
-  }
-  
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -63,6 +59,7 @@ app.post('/api/persons', (request, response) => {
   .then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
--- osa3_puhelinluettelo/backend/models/person.js
+++ osa3_puhelinluettelo/backend/models/person.js
@@ -17,8 +17,16 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,    
+    minlength: 2,    
+    required: true  
+  },
+  number: {
+    type: String,    
+    minlength: 2,    
+    required: true  
+  },
 })
 
 personSchema.set('toJSON', {
