491e37e0484572b821390bd4877dafec1a3c0c46
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-09 14:22:41 +0300

    separete app and index to own files

---
+++ osa4/blogilista/tests/blogs_api.test.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'First Blog',
+    author: 'Author One',
+    url: 'http://example.com/1',
+    likes: 1,
+  },
+  {
+    title: 'Second Blog',
+    author: 'Author Two',
+    url: 'http://example.com/2',
+    likes: 2,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
