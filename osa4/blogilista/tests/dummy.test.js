0c95d8ed8576b0d6800bf8cc5dec15e271f661e0
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-09 13:33:23 +0300

    Create dummy.test.js

---
+++ osa4/blogilista/tests/dummy.test.js
@@ -0,0 +1,8 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
