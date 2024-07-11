28a32091302ba48ab710d6a43c3e79f52e7d5b3f
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-11 09:44:51 +0300

    Update app.js

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 mongoose.set('strictQuery', false)
 
@@ -23,6 +24,7 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 
 module.exports = app
