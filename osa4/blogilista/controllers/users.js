0fa599d6eef5adf5feaed88b113ff163af37cd00
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-14 12:20:12 +0300

    4.16 done

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -3,31 +3,40 @@ const usersRouter = require('express').Router()
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body;
+  const { username, name, password } = request.body
 
-  const existingUser = await User.findOne({ username });
-  if (existingUser) {
-      return response.status(400).json({ error: 'username must be unique' });
+  if (!username || username.length < 3) {
+    return response.status(400).json({ error: 'username must be at least 3 characters long' })
   }
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+  if (!password || password.length < 3) {
+    return response.status(400).json({ error: 'password must be at least 3 characters long' })
+  }
+
+  const existingUser = await User.findOne({ username })
+  if (existingUser) {
+    return response.status(400).json({ error: 'username must be unique' })
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10)
 
   const user = new User({
-      username,
-      name,
-      passwordHash,
+    username,
+    name,
+    passwordHash,
   })
 
-  const savedUser = await user.save();
-  response.status(201).json(savedUser);
+  try {
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    response.status(500).json({ error: 'something went wrong' })
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
   try {
-    const users = await User
-      .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
-
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
     response.json(users)
   } catch (error) {
     response.status(500).json({ error: 'something went wrong' })
