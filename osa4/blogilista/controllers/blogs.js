521661b0a40f1be22b84ccb20d6373110bee94d0
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-14 13:30:44 +0300

    Update blogs.js

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -94,25 +94,28 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
   const token = getTokenFrom(request)
-  let decodedToken
-  try {
-    decodedToken = jwt.verify(token, process.env.SECRET)
-  } catch (error) {
-    return response.status(401).json({ error: 'invalid token' })
+
+  if (!token) {
+    return response.status(401).json({ error: 'token missing' })
   }
 
-  if (!token || !decodedToken.id) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
   try {
     const blog = await Blog.findById(request.params.id)
-    if (blog.user.toString() === decodedToken.id.toString()) {
-      await Blog.findByIdAndDelete(request.params.id)
-      response.status(204).end()
-    } else {
-      response.status(401).json({ error: 'unauthorized user' })
+    if (!blog) {
+      return response.status(404).json({ error: 'blog not found' })
     }
+
+    if (blog.user.toString() !== decodedToken.id.toString()) {
+      return response.status(403).json({ error: 'only the creator can delete the blog' })
+    }
+    
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
   } catch (error) {
     response.status(400).json({ error: 'malformed request' })
   }
