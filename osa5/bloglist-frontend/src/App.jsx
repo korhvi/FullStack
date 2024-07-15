53a08acc6cc7ad3a9f708d9019fa42a7ce33a3f9
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-15 14:36:13 +0300

    ex 5.1 done

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,75 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState('');   
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then(blogs => {
+      setBlogs(blogs);
+    });
+  }, []);
+
+  const handleLogin = async (event) => {    
+    event.preventDefault();    
+    try {      
+      const user = await loginService.login({        
+        username, password,      
+      });      
+      setUser(user);      
+      setUsername('');      
+      setPassword('');    
+    } catch (exception) {      
+      setErrorMessage('wrong credentials');      
+      setTimeout(() => {        
+        setErrorMessage(null);      
+      }, 5000);    
+    } 
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+        {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
