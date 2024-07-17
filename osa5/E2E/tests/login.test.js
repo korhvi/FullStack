eb35d7f05ac748d7c04d13db1f1f49c3d6c252db
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-07-17 16:40:28 +0300

    ex 5.18 done

--- osa5/E2E/tests/login.test.js
+++ osa5/E2E/tests/login.test.js
@@ -6,7 +6,7 @@ describe('Blog app', () => {
     await request.post('http://localhost:3003/api/users', {
       data: {
         name: 'Testi Testinen',
-        username: 'Test',
+        username: 'testausta',
         password: 'salainen'
       }
     })
@@ -17,5 +17,29 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     const loginForm = page.locator('form')
     await expect(loginForm).toBeVisible()
+  })
+  
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.waitForSelector('input[name="Username"]')
+      await page.locator('input[name="Username"]').fill('testausta')
+
+      await page.waitForSelector('input[name="Password"]')
+      await page.locator('input[name="Password"]').fill('salainen')
+
+      await page.locator('button[type="submit"]').click()
+      await expect(page.locator('text=Testi Testinen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.waitForSelector('input[name="Username"]')
+      await page.locator('input[name="Username"]').fill('testausta')
+
+      await page.waitForSelector('input[name="Password"]')
+      await page.locator('input[name="Password"]').fill('vääräsalasana')
+      
+      await page.locator('button[type="submit"]').click()
+      await expect(page.locator('text=wrong username or password')).toBeVisible()
+    })
   })
 })
