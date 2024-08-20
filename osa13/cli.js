ec2598e57b3df5ed17e67a5c35e8987208c78908
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-08-20 12:53:44 +0300

    ex 13.4 done. Added Delete and Post commands

--- osa13/cli.js
+++ osa13/cli.js
@@ -34,18 +34,42 @@ Blog.init({
   modelName: 'blog'
 });
 
-(async () => {
+app.get('/api/blogs', async (req, res) => {
   try {
-    await sequelize.authenticate();
-    
     const blogs = await Blog.findAll();
-
-    blogs.forEach(blog => {
-      console.log(`${blog.author}: '${blog.title}', ${blog.likes} likes`);
-    });
-
-    await sequelize.close();
+    res.json(blogs);
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    res.status(500).json({ error: 'Something went wrong' });
   }
-})();
+});
+
+app.post('/api/blogs', async (req, res) => {
+  const { author, url, title, likes } = req.body;
+
+  try {
+    const newBlog = await Blog.create({ author, url, title, likes });
+    res.status(201).json(newBlog);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const blog = await Blog.findByPk(id);
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    await blog.destroy();
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- osa13/index.js
+++ osa13/index.js
@@ -3,6 +3,8 @@ const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 class Blog extends Model {}
@@ -37,6 +39,32 @@ Blog.init({
 app.get('/api/blogs', async (req, res) => {
   const blogs = await Blog.findAll();
   res.json(blogs);
+});
+
+app.post('/api/blogs', async (req, res) => {
+  const { author, url, title, likes } = req.body;
+  
+  try {
+    const newBlog = await Blog.create({ author, url, title, likes });
+    res.status(201).json(newBlog);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const { id } = req.params;
+  
+  try {
+    const blog = await Blog.findByPk(id);
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found' });
+    }
+    await blog.destroy();
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
