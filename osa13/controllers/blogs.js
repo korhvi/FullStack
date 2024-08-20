35f67f559569aef14c8fcb758b6f6de902822ba8
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-08-20 13:17:05 +0300

    Added Put function

--- osa13/controllers/blogs.js
+++ osa13/controllers/blogs.js
@@ -29,4 +29,22 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { likes } = req.body;
+
+  try {
+    const blog = await Blog.findByPk(id);
+    if (blog) {
+      blog.likes = likes;
+      await blog.save();
+      res.json(blog);
+    } else {
+      res.status(404).json({ error: 'Blog not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
 module.exports = router;
