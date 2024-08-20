1cd248c76e5b85c16aa74656c44202e2bb8c9687
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-08-20 13:05:30 +0300

    Restructered into a modular format

---
+++ osa13/controllers/blogs.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body);
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id);
+    if (blog) {
+      await blog.destroy();
+      res.status(204).end();
+    } else {
+      res.status(404).json({ error: 'Blog not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- osa13/index.js
+++ osa13/index.js
@@ -1,82 +1,19 @@
-require('dotenv').config();
-const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 
+const { PORT } = require('./util/config');
+const { connectToDatabase } = require('./util/db');
+const blogsRouter = require('./controllers/blogs');
+
 app.use(express.json());
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+app.use('/api/blogs', blogsRouter);
 
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.STRING,
-  },
-  url: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-});
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll();
-  res.json(blogs);
-});
-
-app.post('/api/blogs', async (req, res) => {
-  const { author, url, title, likes } = req.body;
-  
-  try {
-    const newBlog = await Blog.create({ author, url, title, likes });
-    res.status(201).json(newBlog);
-  } catch (error) {
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  const { id } = req.params;
-  
-  try {
-    const blog = await Blog.findByPk(id);
-    if (!blog) {
-      return res.status(404).json({ error: 'Blog not found' });
-    }
-    await blog.destroy();
-    res.status(204).end();
-  } catch (error) {
-    res.status(500).json({ error: 'Something went wrong' });
-  }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-(async () => {
-  await sequelize.sync({ force: true });
-
-  await Blog.create({ author: 'Matti Meikäläinen', url: 'http://blogsite.fi/kaunis-syksy', title: 'Kaunis Syksy' });
-  await Blog.create({ author: 'Liisa Virtanen', url: 'http://virtasenajatuksia.fi/viisauden-alkupera', title: 'Viisauden Alkuperä' });
-
-  console.log('Blogs created');
-})();
+start();
